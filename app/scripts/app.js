9e26d6b37c66588bfdaa35f519d516bb458f3687
TJ VanToll <tj.vantoll@gmail.com>
2014-09-23 16:26:03 -0400

    Proposed final state of this tutorial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,31 +10,72 @@
     });
 
     function initialize() {
-        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "flat",
+            transition: "slide"
+        });
         $("#grocery-list").kendoMobileListView({
             dataSource: groceryDataSource,
             template: "#: Name #"
         });
     }
 
-    function closeDialog() {
-        $("#add").data("kendoMobileModalView").close();
-        $("#grocery").val("");
-    }
-
     window.addView = kendo.observable({
         add: function() {
-            var grocery = $("#grocery").val();
-            if (grocery.trim() === "") {
+            if (this.grocery.trim() === "") {
                 navigator.notification.alert("Please provide a grocery.");
                 return;
             }
 
-            groceryDataSource.add({ Name: grocery });
-            groceryDataSource.one("sync", closeDialog);
+            groceryDataSource.add({ Name: this.grocery });
+            groceryDataSource.one("sync", this.close);
             groceryDataSource.sync();
         },
-        close: closeDialog
+        close: function() {
+            $("#add").data("kendoMobileModalView").close();
+            this.grocery = "";
+        }
+    });
+
+    window.loginView = kendo.observable({
+        submit: function() {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.login(this.username, this.password,
+                function(data) {
+                    window.location.href = "#list";
+                    groceryDataSource.read();
+                }, function() {
+                    navigator.notification.alert("Unfortunately we could not find your account.");
+                });
+        }
+    });
+
+    window.registerView = kendo.observable({
+        submit: function() {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, { Email: this.email },
+                function() {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function() {
+                    alert("Unfortunately we were unable to create your account.");
+                });
+        }
     });
 
     document.addEventListener("deviceready", initialize);
